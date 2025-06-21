b16eb5425451093ab8048a664aec7bd0351234c6
Xinul999 <guichardjulien@hotmail.fr>
2025-06-21 19:52:20 +0200

    Test collision

--- Assets/scripts/BoxIdentifier.cs
+++ Assets/scripts/BoxIdentifier.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 public class BoxIdentifier : MonoBehaviour
 {
     public int boxID; //ce sera 1 2 ou 3, on va voir ça dans
-
+    [SerializeField] private Rigidbody boxRigid;
     private void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.TryGetComponent<PlayerController>(out PlayerController player)) {
-            Debug.Log("BoxIdentifier " + boxID);   
+            Debug.Log("BoxIdentifier " + boxID + " détectée par " + player.name);  
+            Vector3 direction = player.transform.position - transform.position;
+            boxRigid.AddForce(direction.normalized * 10f, ForceMode.Impulse);
         }
       
     }
