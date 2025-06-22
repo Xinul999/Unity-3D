f62000cf3949ced413308ab6747d100c610bf5e8
Xinul999 <guichardjulien@hotmail.fr>
2025-06-22 12:13:56 +0200

    Modification collision BoxIdentifier

--- Assets/scripts/BoxIdentifier.cs
+++ Assets/scripts/BoxIdentifier.cs
@@ -5,13 +5,16 @@ public class BoxIdentifier : MonoBehaviour
 {
     public int boxID; //ce sera 1 2 ou 3, on va voir ça dans
     [SerializeField] private Rigidbody boxRigid;
-    private void OnCollisionEnter(Collision other)
+    
+    
+    private void OnTriggerEnter(Collider other)
     {
+        Debug.Log("OnTriggerEnter avec " + other.gameObject.name);
+        
         if (other.gameObject.TryGetComponent<PlayerController>(out PlayerController player)) {
-            Debug.Log("BoxIdentifier " + boxID + " détectée par " + player.name);  
-            Vector3 direction = player.transform.position - transform.position;
-            boxRigid.AddForce(direction.normalized * 10f, ForceMode.Impulse);
+            Debug.Log("BoxIdentifier " + boxID + " (trigger) détectée par " + player.name);  
+            Vector3 direction = transform.position - player.transform.position;
+            boxRigid.AddForce(direction.normalized * 15f, ForceMode.Impulse);
         }
-      
     }
 }
