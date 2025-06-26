aaca6413c83ff4de9e2b17aa062bffc3f4016650
Xinul999 <guichardjulien@hotmail.fr>
2025-06-26 11:15:47 +0200

    Ajoute du texte sur les boites

--- Assets/scripts/BoxIdentifier.cs
+++ Assets/scripts/BoxIdentifier.cs
@@ -1,11 +1,14 @@
 using System;
+using TMPro;
 using UnityEngine;
 
 public class BoxIdentifier : MonoBehaviour
 {
     public int boxID; //ce sera 1 2 ou 3, on va voir ça dans
     [SerializeField] private Rigidbody boxRigid;
-    
+    [SerializeField] private TextMeshPro boxText; // Référence au composant TextMeshPro
+    [SerializeField] private string boxLabel = "Box"; // Texte de base pour l'étiquette
+
     
     private void OnTriggerEnter(Collider other)
     {
@@ -17,4 +20,40 @@ public class BoxIdentifier : MonoBehaviour
             boxRigid.AddForce(direction.normalized * 15f, ForceMode.Impulse);
         }
     }
+
+  
+    private void Start()
+    {
+        if (boxText == null) {
+            boxText = GetComponentInChildren<TextMeshPro>();
+            
+            if (boxText == null) {
+                CreateTextMesh();
+            }
+        }
+        UpdateBoxText();
+    }
+    
+    private void CreateTextMesh()
+    {
+        GameObject textObj = new GameObject("BoxText");
+        textObj.transform.SetParent(transform);
+        textObj.transform.localPosition = new Vector3(0, 1.0f, 0); // Positionner au-dessus de la boîte
+        textObj.transform.localRotation = Quaternion.identity;
+        
+        boxText = textObj.AddComponent<TextMeshPro>();
+        boxText.alignment = TextAlignmentOptions.Center;
+        boxText.fontSize = 6;
+        boxText.color = Color.white;
+        
+        boxText.transform.rotation = Quaternion.LookRotation(boxText.transform.position - Camera.main.transform.position);
+    }
+    
+    private void UpdateBoxText()
+    {
+        if (boxText != null) {
+            boxText.text = boxLabel;
+        }
+    }
+    
 }
